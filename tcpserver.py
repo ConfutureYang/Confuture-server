cf639416317e3c58199700e96a7924537d4cae6e
111 <1@12.com>
2018-03-11 15:25:53 +0800

    refactor one

---
+++ tcpserver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# Copyright 2018 Confuture
+#
+import socket
+import mainioloop
+import socketstream
+
+class TCPServer(object):
+
+    def listen(self,port):
+        host = ("",port)
+        accept_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        accept_socket.bind(host)
+        accept_socket.listen(5)
+        accept_socket.setblocking(False)
+        self.sock = accept_socket
+        self.loop = mainioloop.MainLoop()
+        self.loop.add_handler(self.sock.fileno(),self.handle_connection)
+
+
+    def handle_connection(self,fd,event):
+        try:
+            conn,addr = self.sock.accept()
+        except Exception,e:
+            print "error in TCPServer:{}".format(e)
+        else:
+            sock_stream = socketstream.SocketStream(conn)
+            self.loop.add_handler(conn,sock_stream.handle_event)
+
