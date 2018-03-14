96e5e2d5e58e7ce7fd75e42e1133d022eabad9e7
yangfeihu <yangfeihu@tigerbrokers.com>
2018-03-14 09:58:20 +0800

    目前主要用http_server

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import tcpserver
+#!/usr/bin/env python
+#
+# Copyright 2018 Confuture
+#
+
 import httpserver
 
-tcp_server = tcpserver.TCPServer()
-tcp_server.listen(8888)
-
-# http_server = httpserver.HttpServer()
-# httpserver.listen(8888)
+http_server = httpserver.HttpServer()
+httpserver.listen(8888)
